da1ea92fb16b648c9a2586d951e73f49d5c4f7c8
PT <103441012+wejusdgfsJKA@users.noreply.github.com>
2025-01-05 19:11:40 +0300

    Implemented camera controls.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+    protected float xRot, yRot;
+    protected Vector2 mouseDelta, wasd;
+    protected bool pivoting, highSpeed;
+    public float xSens = 1;
+    public float ySens = 1;
+    public float movSpd = 1;
+    public float spdMultiplier = 2;
+    public float zoomSpd = .01f;
+    protected void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        xRot = yRot = 0;
+    }
+    protected void Update()
+    {
+        if (!pivoting)
+        {
+            Vector3 dir = new Vector3(wasd.x, wasd.y, 0).normalized * movSpd * Time.deltaTime;
+            if (highSpeed)
+            {
+                dir *= spdMultiplier;
+            }
+            transform.Translate(dir, Space.Self);
+        }
+    }
+    public void OnZoom(InputAction.CallbackContext context)
+    {
+        if (!pivoting)
+        {
+            transform.Translate(new Vector3(0, 0, context.ReadValue<float>() * zoomSpd), Space.Self);
+        }
+    }
+    public void OnMouseDelta(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+        if (pivoting)
+        {
+            yRot += mouseDelta.x * ySens;
+            xRot -= mouseDelta.y * xSens;
+            transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+        }
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        wasd = context.ReadValue<Vector2>();
+    }
+    public void OnPivot(InputAction.CallbackContext context)
+    {
+        pivoting = Convert.ToBoolean(context.ReadValue<float>());
+    }
+    public void OnSpeed(InputAction.CallbackContext context)
+    {
+        highSpeed = Convert.ToBoolean(context.ReadValue<float>());
+    }
+}
